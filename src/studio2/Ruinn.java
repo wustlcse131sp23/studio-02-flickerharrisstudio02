1bcf584022ffc4b5aee7dd0ba314d6886490d303
mollyflicker <mollyflicker@10.232.141.9>
2023-02-02 12:48:25 -0600

    in progress

---
+++ src/studio2/Ruinn.java
@@ -0,0 +1,55 @@
+package studio2;
+
+import java.util.Scanner;
+
+public class Ruinn
+
+{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Please enter your starting amount");
+		Scanner in = new Scanner(System.in);
+		int startAmount = in.nextInt();
+int initial = startAmount;
+
+		System.out.println("Please enter your win chance amount");
+		Scanner scan = new Scanner(System.in);
+		double winChance = scan.nextDouble();
+		System.out.println("Please enter your win limit amount");
+		Scanner i = new Scanner(System.in);
+		int winLimit = i.nextInt();
+
+		System.out.println("Please enter total simulations");
+		Scanner e = new Scanner(System.in);
+		int totalSimulations = e.nextInt();
+		
+		for (int j = 1; j <= totalSimulations; j++) {
+			int x = 0;
+	
+			while ((initial != winLimit) && (initial > 0)) {
+
+				if (winChance >= Math.random()) {
+
+					++startAmount;
+
+				}
+
+				else {
+					--startAmount;
+
+				} 
+				++x;
+			}
+
+			if (initial == winLimit) {
+				System.out.println("You have reached your limit." + winLimit + " was made. This was simulation "
+						+ j + "Success!");
+			} else {
+				System.out.println("Ruin! this was simulation " + j + " Better luck next time.");
+			}
+		}
+
+	}
+}
